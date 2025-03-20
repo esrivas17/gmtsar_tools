a8fe9c6c97962f9e6df91098494824950b9eec9a
esrivas17 <esrivas17@gmail.com>
2025-03-20 10:30:50 +0100

    giving orb as text in plot

--- plot_network_from_intf.py
+++ plot_network_from_intf.py
@@ -22,6 +22,7 @@ def main():
 
     yearfrac = data['aligned_time'].to_numpy()
     bperp = data.Bperp.to_numpy()
+    sat_orb = data.sat_orb.to_numpy()
     xmin, xmax = yearfrac.min(), yearfrac.max()
     ymin, ymax = bperp.min(), bperp.max()
 
@@ -51,6 +52,8 @@ def main():
     yticks = np.arange(ymin - (ymin%50), ymax + (100 - ymax%50), 50)
     ax.scatter(yearfrac, bperp, marker='o', c='violet', s=14)
     ax.add_collection(collection)
+    for ix, txt in enumerate(sat_orb):
+            ax.annotate(txt, (yearfrac[ix], bperp[ix]), xytext=(5,5), textcoords='offset points', fontsize='x-small')
     title = f'Num of ifgs: {len(pairs)}'
     ax.set_title(title)
     ax.set_xlabel(f'Time')
