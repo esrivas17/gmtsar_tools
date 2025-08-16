4896d096d0e80b10313123706c53697094146b3e
esrivas17 <esrivas17@gmail.com>
2025-08-16 11:33:55 +0200

    overwrite function

--- plot_slc.py
+++ plot_slc.py
@@ -15,6 +15,8 @@ def main():
     showflag = args.showflag
     savedir = args.savedir
     band = args.band
+    overwriteflag = args.overwrite
+
     if band is None:
         band = ['phase', 'magnitude', 'normmagnitude']
     else:
@@ -36,7 +38,7 @@ def main():
         totalslcs = len(slcfiles)
         for slcfile in slcfiles:
             prmfile = slcfile.with_suffix('.PRM')
-            r = plot_slc(slcfile, prmfile, savedir, band, showflag)
+            r = plot_slc(slcfile, prmfile, savedir, band, showflag, overwriteflag)
             if r != 0:
                 slcfiles_exceptions.append(slcfile)
                 count_badfiles += 1
@@ -47,12 +49,13 @@ def main():
         if count_badfiles > 0:
             print(f'SLC files with issues: \n{"\n".join(slcfiles_exceptions)}')
         return 0
+    
     elif filepath:
         if filepath.suffix != '.SLC':
             raise(f'File path seems not to be a SLC file: {filepath}')
 
         prmfile = filepath.with_suffix('.PRM')
-        r = plot_slc(filepath, prmfile, savedir, band, showflag)
+        r = plot_slc(filepath, prmfile, savedir, band, showflag, overwriteflag)
         if r != 0:
             raise Exception(f'Something wrong with plot_slc function\nArguments: {filepath, prmfile, savedir}')
 
@@ -61,10 +64,57 @@ def main():
         raise Exception(f'Why I am here?')
 
 
-def plot_slc(slcfile:Path, prmfile:Path, savedir:Path, bands: list, showflag=False):
+#def open_gmtsar_slc(slcfile:Path, prmfile:Path):
+
+
+def plot_slc(slcfile:Path, prmfile:Path, savedir:Path, bands: list, showflag=False, overwriteflag=False):
+    if 'phase' in bands:
+        phasefilename = savedir.joinpath(slcfile.stem + '_pha.png')
+        if phasefilename.exists():
+            print(f'File: {phasefilename} already exists')
+            if overwriteflag:
+                print("Overwriting plot...")
+                plotphase = True
+            else:
+                plotphase = False
+        else:
+            plotphase =True
+    else:
+        plotphase = False
+
+    if 'magnitude' in bands:
+        magnitudefilename = savedir.joinpath(slcfile.stem + '_mag.png')
+        if magnitudefilename.exists():
+            print(f'File: {magnitudefilename} already exists')
+            if overwriteflag:
+                print("Overwriting plot...")
+                plotmagn = True
+            else:
+                plotmagn = False
+        else:
+            plotmagn =True
+    else:
+        plotmagn = False
+
+    if 'normmagnitude' in bands:
+        normalizedmagfilename = savedir.joinpath(slcfile.stem + '_norm.png')
+        if normalizedmagfilename.exists():
+            print(f'File: {normalizedmagfilename} already exists')
+            if overwriteflag:
+                print("Overwriting plot...")
+                plotnormmag = True
+            else:
+                plotnormmag = False
+        else:
+            plotnormmag =True
+    else:
+        plotnormmag = False
+
+    if not any([plotphase, plotmagn, plotnormmag]):
+        print(f'Skipping plots..')
+        return 0
+    
     slcstem = slcfile.stem
-    #slcfile = str(slcfile)
-    #prmfile = str(prmfile)
     print(f'PRM file: {prmfile}')
     #Get rows and columns
     nlines = grep(prmfile, 'num_lines')
@@ -95,19 +145,18 @@ def plot_slc(slcfile:Path, prmfile:Path, savedir:Path, bands: list, showflag=Fal
     print("="*20,"Plotting","="*20)
 
     # Phase
-    if 'phase' in bands:
+    if plotphase:
         fig_phase, ax_phase = plt.subplots(1,1, figsize=(10,8))
         cax_phase = ax_phase.imshow(phase, cmap="hsv", vmin=-np.pi, vmax=np.pi, aspect="auto")
         ax_phase.set_title(f'Phase: {slcstem}')
         ax_phase.set_ylabel('Range')
         ax_phase.set_xlabel('Azimuth')
         fig_phase.colorbar(cax_phase, ax=ax_phase, label='rad')
-        phasefilename = savedir.joinpath(slcfile.stem + '_pha.png')
         fig_phase.savefig(f'{phasefilename}')
         print(f'Phase plot save as: {phasefilename}')
 
     # Magnitude
-    if 'magnitude' in bands:
+    if plotmagn:
         fig_mag, ax_mag = plt.subplots(1,1, figsize=(10,8))
         vmin, vmax = np.percentile(magnitude_log, [1, 99])  # Clip 1st and 99th percentile
         cax_mag = ax_mag.imshow(magnitude_log, cmap="gray", aspect="auto", vmin=vmin, vmax=vmax)
@@ -115,12 +164,11 @@ def plot_slc(slcfile:Path, prmfile:Path, savedir:Path, bands: list, showflag=Fal
         ax_mag.set_ylabel('Range')
         ax_mag.set_xlabel('Azimuth')
         fig_mag.colorbar(cax_mag, ax=ax_mag)
-        magnitudefilename = savedir.joinpath(slcfile.stem + '_mag.png')
         fig_mag.savefig(f'{magnitudefilename}')
         print(f'Magnitude plot save as: {magnitudefilename}')
 
     # Normalized Magnitude
-    if 'normmagnitude' in bands:
+    if plotnormmag:
         norm_magnitude = magnitude / magnitude.max()  # Scale between 0 and 1
         norm_magnitude_log = np.log1p(norm_magnitude * 1000)  # Scale to improve visibility
         fig_nmag, ax_nmag = plt.subplots(1,1, figsize=(10,8))
@@ -129,7 +177,6 @@ def plot_slc(slcfile:Path, prmfile:Path, savedir:Path, bands: list, showflag=Fal
         ax_nmag.set_ylabel('Range')
         ax_nmag.set_xlabel('Azimuth')
         fig_nmag.colorbar(cax_nmag, ax=ax_nmag)
-        normalizedmagfilename = savedir.joinpath(slcfile.stem + '_norm.png')
         fig_nmag.savefig(f'{normalizedmagfilename}')
         print(f'Normalized magnitude plot save as: {normalizedmagfilename}\n')
 
@@ -164,7 +211,7 @@ def get_args():
     parser.add_argument('-s', '--savedir', type=Path, dest='savedir', help='Path to directory to save figures')
     parser.add_argument('--showflag', action='store_true', default=False, help='show flag')
     parser.add_argument('--band', dest='band', choices=['phase', 'magnitude', 'normmagnitude'], default=None, help='Choose what to plot, choices: phase, magnitude, normmagnitude. By default it plots the 3 f them')
-
+    parser.add_argument('--ow', dest='overwrite', action='store_true', default=False, help='If set overwrites plots, default False')
     return parser.parse_args()
 
 
